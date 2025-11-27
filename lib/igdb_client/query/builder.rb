b57b4cc2f6586f04c127b771a96d1052c9563b7e
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2025-11-26 21:57:01 -0500

    Fix Query::Builder Cyclomatic Complexity

--- lib/igdb_client/query/builder.rb
+++ lib/igdb_client/query/builder.rb
@@ -32,19 +32,31 @@ module IgdbClient
       private
 
       def perform_validations
-        errors = []
+        @errors = []
+        validate_id_without_search
+        validate_fields_without_excludes
+        validate_id_without_offset
+        validate_id_without_filters
 
-        errors << "Cannot combine ID with Search" if @id.present? && @search.present?
-
-        errors << "Cannot combine Fields with Exclude" if @fields != "*" && @exclude.present?
-
-        errors << "Cannot combine ID with Offset" if @id.present? && @offset.present?
-
-        errors << "Cannot combine ID with Filters" if @id.present? && @filter.present?
-
-        raise InvalidArguments, errors.join(", ") if errors.any?
+        raise InvalidArguments, @errors.join(", ") if @errors.any?
 
         show_redundant_argument_warning if @id.present? && @limit.present?
+      end
+
+      def validate_id_without_search
+        @errors << "Cannot combine ID with Search" if @id.present? && @search.present?
+      end
+
+      def validate_fields_without_excludes
+        @errors << "Cannot combine Fields with Exclude" if @fields != "*" && @exclude.present?
+      end
+
+      def validate_id_without_offset
+        @errors << "Cannot combine ID with Offset" if @id.present? && @offset.present?
+      end
+
+      def validate_id_without_filters
+        @errors << "Cannot combine ID with Filters" if @id.present? && @filter.present?
       end
 
       def params
